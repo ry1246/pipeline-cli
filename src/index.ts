c1cec98164e6eeb885edf187d47bca6438fabe33
Ryoma Hara <toooooom720@gmail.com>
2026-09-13 22:53:39 +0900

    feat: commanderでCLIの土台を作成（--version, --help）

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("mylogfilter")
+  .description("パイプで渡されたログをフィルタするCLI")
+  .version("1.0.0");
+
+program.parse();
