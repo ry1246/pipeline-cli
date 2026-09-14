c0eb87ad989eaa61a99858a1c3422ef4010ce3a4
Ryoma Hara <toooooom720@gmail.com>
2026-09-14 22:31:08 +0900

    feat: -p/--pattern オプションで正規表現による行フィルタを実装

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,18 @@ const program = new Command();
 program
   .name("mylogfilter")
   .description("パイプで渡されたログをフィルタするCLI")
-  .version("1.0.0");
+  .version("1.0.0")
+  .option("-p, --pattern <regex>", "指定した正規表現にマッチする行だけを出力");
 
 program.parse();
+
+const { pattern } = program.opts();
+const re = pattern ? new RegExp(pattern) : undefined;
 
 const rl = createInterface({ input: process.stdin });
 
 rl.on("line", (line) => {
-  console.log(line)
+  if (!re || re.test(line)) {
+    console.log(line);
+  }
 });
