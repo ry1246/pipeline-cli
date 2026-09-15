17b8db56451e2a76c540b981f7e23bbbe3e74fb9
Ryoma Hara <toooooom720@gmail.com>
2026-09-15 23:18:52 +0900

    feat: -i/-v オプションで大文字小文字無視・マッチ反転を実装

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,20 @@ program
   .name("mylogfilter")
   .description("パイプで渡されたログをフィルタするCLI")
   .version("1.0.0")
-  .option("-p, --pattern <regex>", "指定した正規表現にマッチする行だけを出力");
+  .option("-p, --pattern <regex>", "指定した正規表現にマッチする行だけを出力")
+  .option("-i, --ignore-case", "パターンまっちを大文字小文字無視で行う")
+  .option("-v, --invert-match", "マッチしなかったら行を出力する（grepの-vと同じ）");
 
 program.parse();
 
-const { pattern } = program.opts();
-const re = pattern ? new RegExp(pattern) : undefined;
+const { pattern, ignoreCase, invertMatch } = program.opts();
+const re = pattern ? new RegExp(pattern, ignoreCase ? "i" : undefined) : undefined;
 
 const rl = createInterface({ input: process.stdin });
 
 rl.on("line", (line) => {
-  if (!re || re.test(line)) {
+  const matched = !re || re.test(line);
+  if (matched !== !!invertMatch) {
     console.log(line);
   }
 });
